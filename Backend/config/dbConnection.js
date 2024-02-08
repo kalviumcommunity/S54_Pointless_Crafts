b46eb2bc5b1a836709ba484e77ec4603a056493f
shivangi-jain-08 <shivangi.jain@kalvium.community>
2024-02-08 09:39:35 +0530

    Added MongoDB integration and database connection status to home route

--- Backend/config/dbConnection.js
+++ Backend/config/dbConnection.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const connectDB = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGO_URL)
-        console.log("📦 Database Connected", connection.connection.name, connection.connection.host);
-    } catch (error) {
-        console.log("error: ", error);
-        process.exit(1);
-    }
-};
 
-module.exports = connectDB
+
+function connectToDB() {
+    return mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+}
+
+module.exports = connectToDB;
