b0c32a7becd684577d7ed3a4ea1d0ff42049d880
shivangi-jain-08 <shivangi.jain@kalvium.community>
2024-03-22 14:54:50 +0530

    added userRoute to server.js

--- Backend/data/userData.js
+++ Backend/data/userData.js
@@ -10,64 +10,64 @@ const User1 = new User({
     favPosts: [],
 })
 const User2 = new User({
-    userName: "user1",
-    name: "Aplha",
+    userName: "user2",
+    name: "Sigma",
     followers: 2,
     following: 1,
     favPosts: [],
 })
 const User3 = new User({
-    userName: "user1",
-    name: "Aplha",
+    userName: "user3",
+    name: "Lambda",
     followers: 2,
     following: 1,
     favPosts: [],
 })
 const User4 = new User({
-    userName: "user1",
-    name: "Aplha",
+    userName: "user4",
+    name: "zeta",
     followers: 2,
     following: 1,
     favPosts: [],
 })
 const User5 = new User({
-    userName: "user1",
-    name: "Aplha",
+    userName: "user5",
+    name: "Epsilon",
     followers: 2,
     following: 1,
     favPosts: [],
 })
 const User6 = new User({
-    userName: "user1",
-    name: "Aplha",
+    userName: "user6",
+    name: "Theta",
     followers: 2,
     following: 1,
     favPosts: [],
 })
 const User7 = new User({
-    userName: "user1",
-    name: "Aplha",
+    userName: "user7",
+    name: "omega",
     followers: 2,
     following: 1,
     favPosts: [],
 })
 const User8 = new User({
-    userName: "user1",
-    name: "Aplha",
+    userName: "user8",
+    name: "delta",
     followers: 2,
     following: 1,
     favPosts: [],
 })
 const User9 = new User({
-    userName: "user1",
-    name: "Aplha",
+    userName: "user9",
+    name: "gamma",
     followers: 2,
     following: 1,
     favPosts: [],
 })
 const User10 = new User({
-    userName: "user1",
-    name: "Aplha",
+    userName: "user10",
+    name: "beta",
     followers: 2,
     following: 1,
     favPosts: [],
@@ -79,6 +79,6 @@ mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("Connected to MongoDB database"))
     .catch((error) => console.error("Failed to connect to MongoDB:", error));
 
-// User.insertMany(userData, { timeout: 30000 })
-//     .then(() => console.log("User list added"))
-//     .catch((error) => console.log("Failed to insert data:", error));
+User.insertMany(userData, { timeout: 30000 })
+    .then(() => console.log("User list added"))
+    .catch((error) => console.log("Failed to insert data:", error));
